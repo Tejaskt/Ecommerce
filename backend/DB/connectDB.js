b7631652766e606133c4512b679c33b3c9c6f8f4
Tejaskt <tejasktgeetanjali88@gmail.com>
2025-06-23 18:47:32 +0530

    mongodb, register, login

--- backend/DB/connectDB.js
+++ backend/DB/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectDB(){
 
-    mongoose.connect('mongodb://127.0.0.1:27017/test')
+    mongoose.connect('mongodb://127.0.0.1:27017/MCA')
     .then(()=>{
         console.log("DB is connected !!")
     }).catch(()=>{
